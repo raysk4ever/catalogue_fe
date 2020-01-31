cb33ac986a34cf936f034818475ab2883c4e1ff5
Ravi Kant Singh <raysk7161@gmail.com>
2020-02-01 02:26:43 +0530

    added react router, redux, bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-container">
+        <p>Nav</p>
+      </div>
+      <div clasName="body-container">
+        <div>
+          <div className="side-bar-container">
+            <p>Side bar</p>           
+          </div>
+          <div className="body-content-container">
+            <p>Body</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
